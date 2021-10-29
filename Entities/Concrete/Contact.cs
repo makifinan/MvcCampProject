841d6d0bd1792ede71835205614d55fc40ab2a31
makifinan <eposta@adresiniz.comm.akifinan@outlook.com.tr>
2021-10-29 20:12:47 +0300

    Add project files.

---
+++ Entities/Concrete/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+
+        [StringLength(30)]
+        public string UserName { get; set; }
+        
+        [StringLength(50)]
+        public string UserMail { get; set; }
+        
+        [StringLength(50)]
+        public string Subject { get; set; }
+        
+        [StringLength(1000)]
+        public string Message { get; set; }
+    
+    }
+
+}
---
+++ Entities/Concrete/Writer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Writer
+    {
+        [Key]
+        public int WriterId { get; set; }
+
+        [StringLength(50)]
+        public string WriteFirstName { get; set; }
+        
+        [StringLength(50)]
+        public string WriteLastName { get; set; }
+        
+        [StringLength(100)]
+        public string WriteImage { get; set; }
+        
+        [StringLength(50)]
+        public string WriteMail { get; set; }
+        
+        [StringLength(25)]
+        public string WritePassword { get; set; }
+
+        public ICollection<Heading> Headings { get; set; }
+        public ICollection<Content> Contents { get; set; }
+
+
+
+    }
+}
